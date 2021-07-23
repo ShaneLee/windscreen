d7e2b97a823fe01625571f966926ffc09a2211b8
Shane <me@shanel.ee>
2021-07-23 15:57:57 +0100

    Update parser

--- src/parser.py
+++ src/parser.py
@@ -1,5 +1,8 @@
 from bs4 import BeautifulSoup as bs
 
+from src.models.metadata import Metadata
+from src.builder.metadata_builder import MetadataBuilder
+
 class Parser:
     def __init__(self):
         pass
@@ -17,4 +20,26 @@ class Parser:
         return set(
             filter(lambda x: 'content' in x, 
             map(lambda x: x.get('href'), links)))
+
+    def find_metadata(self, html):
+        soup = bs(html, 'html.parser')
+        return MetadataBuilder(True).with_url(self._find_url(soup)) .with_headline(self._find_headline(soup)) .with_standfirst(self._find_standfirst(soup)) .with_date_published(self._find_date_published(soup)) .with_author(self._find_author(soup)) .with_category(self._find_category(soup)) .build()
+
+    def _find_url(self, soup):
+        return soup.find('link', {'rel': 'canonical'})['href']
         
+    def _find_headline(self, soup):
+        return soup.find('h1', {'class': 'headline'}).text.strip()
+
+    def _find_standfirst(self, soup):
+        return soup.find('h2', {'class': 'article-standfirst'}).text.strip()
+
+    def _find_date_published(self, soup):
+        return soup.find('time', {'itemprop': 'datePublished'})['datetime']
+
+    def _find_author(self, soup):
+        return soup.find('a', {'class':
+        'article-author-byline__author'}).text.strip()
+
+    def _find_category(self, soup):
+        return soup.find('h2', {'class': 'primary-theme'}).text.strip()
--- test/test_parser.py
+++ test/test_parser.py
@@ -1,9 +1,10 @@
 import unittest
-import pkgutil
 
 from src.parser import Parser
 
-FILE = pkgutil.get_data(__name__, "test.html")
+from test.provider.test_file_provider import TestFileProvider
+
+FILE = TestFileProvider().get()
 
 subject = Parser()
 
@@ -13,6 +14,23 @@ class TestParser(unittest.TestCase):
 
     def test_links(self):
        self.assertEqual(16, len(subject.find_unique_article_links(FILE)))
+
+    def test_find_metadata(self):
+       url = 'https://www.ft.com/content/afc67ffb-3f4c-4b41-8240-1a96115325c9'
+       headline = 'It’s not pent up demand, it’s savings'
+       standfirst = 'Also, why did Jamie Dimon get a random $50m?'
+       date_published = '2021-07-22T05:30:07.739Z'
+       author = 'Robert Armstrong'
+       category = 'Markets'
+       result = subject.find_metadata(FILE)
+
+       self.assertEqual(url, result.get_url())
+       self.assertEqual(headline, result.get_headline())
+       self.assertEqual(standfirst, result.get_standfirst())
+       self.assertEqual(date_published, result.get_date_published())
+       self.assertEqual(author, result.get_author())
+       self.assertEqual(category, result.get_category())
+        
 
 if __name__ == '__main__':
     unittest.main()
