4bc33d5dd0713ddecbdc557ad1ccc3b95fc7579e
Shane <me@shanel.ee>
2021-07-24 10:55:52 +0100

    Filter unwanted links

--- src/link_service.py
+++ src/link_service.py
@@ -5,9 +5,13 @@ class LinkService:
 
     def process(self, html):
         links = list(
-            map(self._translate, self.parser.find_unique_article_links(html)))
+            filter(self._remove_unwanted,
+            map(self._translate, self.parser.find_unique_article_links(html))))
         print('retrieved ', len(links), ' links')
         return self.repository.put_all(links)
+
+    def _remove_unwanted(self, link):
+        return 'ft.com/content/' in link
 
     def _translate(self, link):
         return link.replace('www', 'amp')
--- test/test_link_service.py
+++ test/test_link_service.py
@@ -8,7 +8,7 @@ from test.mocks.mock_repository import MockRepository
 MOCK_PARSER = MockParser()
 MOCK_REPOSITORY = MockRepository()
 
-LINKS = ['1','2','3']
+LINKS = ['ft.com/content/1','ft.com/content/2','ft.com/content/3']
 
 subject = LinkService(MOCK_PARSER, MOCK_REPOSITORY)
 
@@ -16,6 +16,10 @@ class TestLinkService(unittest.TestCase):
     def test_process(self):
        MOCK_PARSER.when_find_unique_links_then_return(LINKS)
        self.assertEqual(3, subject.process('html'))
+
+    def test_it_filters_unwanted_links(self):
+       MOCK_PARSER.when_find_unique_links_then_return(['0'])
+       self.assertEqual(0, subject.process('html'))
 
 if __name__ == '__main__':
     unittest.main()
